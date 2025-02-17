9306a898c9b2583b61f03c45c17f4aead0d64ff6
ramadhanharo <ramadhan.haro@moringaschool.com>
2025-02-17 19:44:53 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0:
+        print([])
+        return []
+    if length == 1:
+        print([0])
+        return [0]
+    if length == 2:
+        print([0, 1])
+        return [0, 1]
+    if length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+        return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     pass
